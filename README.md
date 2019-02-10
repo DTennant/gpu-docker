6acdf35a75e13a7ae434a47fc220a85e09e65271
DTennant <Tennant_1999@outlook.com>
2019-02-10 19:57:39 +0800

    update

--- README.md
+++ README.md
@@ -2,7 +2,14 @@ follow the instruction
 [here](https://github.com/nvidia/nvidia-container-runtime#daemon-configuration-file) to change default docker run-time to nvidia-docker
 
 ```shell
+export UID=$(id -u $USER)
+export GID=$(id -g $USER)
+# or you can write this into .env
 docker-compose up -d
 ssh zbc@localhost -p 8222 # passwd: zbc
 ```
+
+### TODO:
+
+- [ ] jupyter passwd
 
--- docker/user.sh
+++ docker/user.sh
@@ -2,16 +2,17 @@
 set -e
  
 printf "\n\033[0;44m---> Creating SSH master user.\033[0m\n"
- 
-useradd -m -d /home/${SSH_MASTER_USER} -G ssh ${SSH_MASTER_USER} -s /bin/bash
+
+USER_ID=${UID:-9001}
+
+useradd -l -u "$USER_ID" -m -d /home/${SSH_MASTER_USER} -G ssh ${SSH_MASTER_USER} -s /bin/bash
+printf "\n\033[0;44m---> user added. \033[0m\n"
 echo "${SSH_MASTER_USER}:${SSH_MASTER_PASS}" | chpasswd
 echo 'PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin"' >> /home/${SSH_MASTER_USER}/.profile
- 
-echo "${SSH_MASTER_USER} ALL=NOPASSWD:/bin/rm" >> /etc/sudoers
-echo "${SSH_MASTER_USER} ALL=NOPASSWD:/bin/mkdir" >> /etc/sudoers
-echo "${SSH_MASTER_USER} ALL=NOPASSWD:/bin/chown" >> /etc/sudoers
-echo "${SSH_MASTER_USER} ALL=NOPASSWD:/usr/sbin/useradd" >> /etc/sudoers
-echo "${SSH_MASTER_USER} ALL=NOPASSWD:/usr/sbin/deluser" >> /etc/sudoers
-echo "${SSH_MASTER_USER} ALL=NOPASSWD:/usr/sbin/chpasswd" >> /etc/sudoers
- 
+echo "${SSH_MASTER_USER} ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+printf "\n\033[0;44m---> PATH and sudo set \033[0m\n"
+
+chown -R ${SSH_MASTER_USER} /home/${SSH_MASTER_USER}
+
+
 exec "$@"
